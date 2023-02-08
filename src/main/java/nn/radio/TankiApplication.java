388313df8b38de9a133634f1fcfc252fe71b05e3
voyagerdva <mariner-9@yandex.ru>
2023-02-08 11:05:10 +0300

    Add tank_config and ConfigReader for start tanks configuration 2

--- src/main/java/nn/radio/TankiApplication.java
+++ src/main/java/nn/radio/TankiApplication.java
@@ -5,11 +5,6 @@ import nn.radio.dto.UserDto;
 import nn.radio.server.ServerThread;
 import nn.radio.server.connection.ClientListenerThread;
 
-//import java.io.BufferedReader;
-//import java.io.File;
-//import java.io.FileReader;
-//import java.io.IOException;
-//import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
